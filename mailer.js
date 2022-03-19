cd03d9336d118d65d99dcab222d3eb18381bea9c
Dmitrii <dmitrii.cherkasov.030298@gmail.com>
2022-03-19 18:18:44 +0300

    Debugging Mailer

--- mailer.js
+++ mailer.js
@@ -8,13 +8,14 @@ async function sendMail(recipient, language, confirmationLink) {
       to: recipient, // Change to your recipient
       from: 'noreply.m1cr0chat@gmail.com', // Change to your verified sender
       subject: 'Confirm our email address',
-      html: `<h2>Welcome to M1cr0chat!</h2><br><a href="${confirmationLink}" target="_blank">Confirm email by this link</a><br><p>Or copy link below:</p><br>${confirmationLink}`,
+      html: `<h2>Welcome to {here will be chat name}!</h2><br><a href="${confirmationLink}" target="_blank">Confirm email by this link</a><br><p>Or copy link below:</p><br>${confirmationLink}`,
     }
   }
 
   await sgMail
   .send(msg)
   .then((response) => {
+    console.log(response)
     if (response[0].statusCode == 202)
     return true
   })
